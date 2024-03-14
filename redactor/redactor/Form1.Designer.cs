2145ef5e96e6445b874dd0a7963916ffcf7bb899
linaliswii <liswiii@yandex.ru>
2024-03-14 21:34:45 +0300

    redactor
    redactor

---
+++ redactor/redactor/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace redactor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.правкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.копироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вставитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.форматированиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.жирныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnFindReplace = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.textBoxReplace = new System.Windows.Forms.TextBox();
+            this.replacebtn = new System.Windows.Forms.Button();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.правкаToolStripMenuItem,
+            this.форматированиеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // правкаToolStripMenuItem
+            // 
+            this.правкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.копироватьToolStripMenuItem,
+            this.вставитьToolStripMenuItem});
+            this.правкаToolStripMenuItem.Name = "правкаToolStripMenuItem";
+            this.правкаToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.правкаToolStripMenuItem.Text = "Правка";
+            // 
+            // копироватьToolStripMenuItem
+            // 
+            this.копироватьToolStripMenuItem.Name = "копироватьToolStripMenuItem";
+            this.копироватьToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.копироватьToolStripMenuItem.Text = "Копировать";
+            this.копироватьToolStripMenuItem.Click += new System.EventHandler(this.копироватьToolStripMenuItem_Click);
+            // 
+            // вставитьToolStripMenuItem
+            // 
+            this.вставитьToolStripMenuItem.Name = "вставитьToolStripMenuItem";
+            this.вставитьToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.вставитьToolStripMenuItem.Text = "Вставить";
+            this.вставитьToolStripMenuItem.Click += new System.EventHandler(this.вставитьToolStripMenuItem_Click);
+            // 
+            // форматированиеToolStripMenuItem
+            // 
+            this.форматированиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.жирныйToolStripMenuItem});
+            this.форматированиеToolStripMenuItem.Name = "форматированиеToolStripMenuItem";
+            this.форматированиеToolStripMenuItem.Size = new System.Drawing.Size(146, 24);
+            this.форматированиеToolStripMenuItem.Text = "Форматирование";
+            // 
+            // жирныйToolStripMenuItem
+            // 
+            this.жирныйToolStripMenuItem.Name = "жирныйToolStripMenuItem";
+            this.жирныйToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.жирныйToolStripMenuItem.Text = "Настройки шрифта";
+            this.жирныйToolStripMenuItem.Click += new System.EventHandler(this.жирныйToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnFindReplace
+            // 
+            this.btnFindReplace.BackColor = System.Drawing.Color.LightPink;
+            this.btnFindReplace.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFindReplace.Location = new System.Drawing.Point(677, 102);
+            this.btnFindReplace.Name = "btnFindReplace";
+            this.btnFindReplace.Size = new System.Drawing.Size(111, 37);
+            this.btnFindReplace.TabIndex = 2;
+            this.btnFindReplace.Text = "Найти ";
+            this.btnFindReplace.UseVisualStyleBackColor = false;
+            this.btnFindReplace.Click += new System.EventHandler(this.btnFindReplace_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(598, 64);
+            this.textBoxSearch.Multiline = true;
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(190, 32);
+            this.textBoxSearch.TabIndex = 3;
+            // 
+            // textBoxReplace
+            // 
+            this.textBoxReplace.Location = new System.Drawing.Point(598, 189);
+            this.textBoxReplace.Multiline = true;
+            this.textBoxReplace.Name = "textBoxReplace";
+            this.textBoxReplace.Size = new System.Drawing.Size(190, 32);
+            this.textBoxReplace.TabIndex = 4;
+            // 
+            // replacebtn
+            // 
+            this.replacebtn.BackColor = System.Drawing.Color.LightPink;
+            this.replacebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.replacebtn.Location = new System.Drawing.Point(677, 227);
+            this.replacebtn.Name = "replacebtn";
+            this.replacebtn.Size = new System.Drawing.Size(111, 38);
+            this.replacebtn.TabIndex = 5;
+            this.replacebtn.Text = "Заменить";
+            this.replacebtn.UseVisualStyleBackColor = false;
+            this.replacebtn.Click += new System.EventHandler(this.replacebtn_Click);
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(221, 18);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.CharHeight = 18;
+            this.fastColoredTextBox1.CharWidth = 10;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.CSharp;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(12, 31);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(541, 400);
+            this.fastColoredTextBox1.TabIndex = 6;
+            this.fastColoredTextBox1.Text = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Zoom = 100;
+            this.fastColoredTextBox1.Load += new System.EventHandler(this.fastColoredTextBox1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fastColoredTextBox1);
+            this.Controls.Add(this.replacebtn);
+            this.Controls.Add(this.textBoxReplace);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.btnFindReplace);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem правкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem копироватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вставитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem форматированиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem жирныйToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnFindReplace;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.TextBox textBoxReplace;
+        private System.Windows.Forms.Button replacebtn;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+    }
+}
+
---
+++ redactor/redactor/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace redactor
+{
+    public partial class Form1 : Form
+    {
+        private string[] csharp = { "using", "namespace", "class", "public", "private", "static", "void", "int", "string" };
+        private string[] java = { "import", "package", "class", "public", "private", "static", "void", "int", "String" };
+        private string[] pascal = { "unit", "interface", "implementation", "uses", "type", "class", "procedure", "function", "var", "begin", "end" };
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Text File(*.txt)|*.txt";
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string fn = saveFileDialog1.FileName;
+            File.WriteAllText(fn,fastColoredTextBox1.Text);
+            MessageBox.Show("Файл успешно сохранен!");
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string fn = openFileDialog1.FileName;
+            string tf = File.ReadAllText(fn);
+            fastColoredTextBox1.Text = tf;
+            MessageBox.Show("Файл открыт!");
+        }
+
+        private void копироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(fastColoredTextBox1.TextLength > 0)
+            {
+                fastColoredTextBox1.Copy();
+            }   
+        }
+
+        private void вставитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (fastColoredTextBox1.TextLength > 0)
+            {
+                fastColoredTextBox1.Paste();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            HighlightSyntax();
+        }
+
+        private void жирныйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            fastColoredTextBox1.Font = fontDialog1.Font;
+        }
+
+        private void HighlightSyntax()
+        {   
+            string code = fastColoredTextBox1.Text;
+
+            // Сброс цвета текста
+            fastColoredTextBox1.SelectionLength = code.Length;
+            fastColoredTextBox1.SelectionColor = SystemColors.WindowText;
+
+            HighlightKeywords(csharp, Color.Blue);
+            HighlightKeywords(java, Color.DarkGreen);
+            HighlightKeywords(pascal, Color.DarkRed);
+        }
+
+        private void HighlightKeywords(string[] keywords, Color color)
+        {
+            foreach (string keyword in keywords)
+            {
+                int index = 0;
+                while (index < fastColoredTextBox1.Text.Length && (index = fastColoredTextBox1.Text.IndexOf(keyword, index, StringComparison.OrdinalIgnoreCase)) != -1)
+                {
+                    fastColoredTextBox1.SelectionStart = index;
+                    fastColoredTextBox1.SelectionLength = keyword.Length;
+                    fastColoredTextBox1.SelectionColor = color; 
+                    index += keyword.Length;
+                }
+            }
+        }
+
+        private void btnFindReplace_Click(object sender, EventArgs e)
+        {
+            string searchText = textBoxSearch.Text;
+
+            if (searchText.Length == 0)
+            {
+                MessageBox.Show("Введите текст для поиска", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            StringComparison comparison = StringComparison.OrdinalIgnoreCase; // Устанавливаем игнорирование регистра
+
+            int index = fastColoredTextBox1.Text.IndexOf(searchText, comparison);
+
+            if (index != -1)
+            {
+                fastColoredTextBox1.SelectionStart = index;
+                fastColoredTextBox1.SelectionLength = searchText.Length;
+                fastColoredTextBox1.Focus(); // Устанавливаем фокус на FastColoredTextBox, чтобы выделение было видимым
+            }
+            else
+            {
+                MessageBox.Show("Текст не найден", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void replacebtn_Click(object sender, EventArgs e)
+        {
+            string searchText = textBoxSearch.Text;
+            string replaceText = textBoxReplace.Text;
+
+            if (searchText.Length == 0)
+            {
+                MessageBox.Show("Введите текст для поиска", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            StringComparison comparison = StringComparison.OrdinalIgnoreCase; // Устанавливаем игнорирование регистра
+
+            int index = fastColoredTextBox1.Text.IndexOf(searchText, comparison);
+
+            if (index != -1)
+            {
+                fastColoredTextBox1.SelectionStart = index;
+                fastColoredTextBox1.SelectionLength = searchText.Length;
+                fastColoredTextBox1.Focus(); // Устанавливаем фокус на FastColoredTextBox, чтобы выделение было видимым
+
+                if (!string.IsNullOrEmpty(replaceText))
+                {
+                    // Если есть текст для замены, заменяем выделенный текст
+                    fastColoredTextBox1.SelectedText = replaceText;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Текст не найден", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fastColoredTextBox1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
